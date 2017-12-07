4c3d0d04659c5f7b1312bae744afe8bc9f1531c7
Larry Xu <hilarryxu@gmail.com>
2017-12-07 18:18:38 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,45 @@ if [ ! -d "pack/bundle" ]; then
   mkdir -p pack/themes/opt
 fi
 
-if [ ! -d "pack/bundle/opt/vim-shortcut" ]; then
-  git clone https://github.com/sunaku/vim-shortcut.git pack/bundle/opt/vim-shortcut
-fi
+function Plug() {
+  local repo=$1
+  local name=`echo $repo | cut -f 2 -d /`
+  local path=pack/bundle/start/${name}
+  if [ ! -d "$path" ]; then
+    git clone https://github.com/$repo.git $path
+  fi
+}
 
-if [ ! -d "pack/bundle/start/vim-mucomplete" ]; then
-  git clone https://github.com/lifepillar/vim-mucomplete.git pack/bundle/start/vim-mucomplete
-fi
+Plug hilarryxu/xcc.vim
+Plug hilarryxu/tag-preview.vim
+Plug hilarryxu/LeaderF-funky
+
+Plug ctrlpvim/ctrlp.vim
+Plug Yggdroot/LeaderF
+Plug lifepillar/vim-mucomplete
+Plug SirVer/ultisnips
+Plug honza/vim-snippets
+Plug t9md/vim-choosewin
+Plug skywind3000/asyncrun.vim
+
+Plug tpope/vim-commentary
+Plug tpope/vim-surround
+Plug tpope/vim-repeat
+Plug tpope/vim-scriptease
+Plug tpope/tpope-vim-abolish
+Plug tpope/vim-unimpaired
+Plug tpope/vim-eunuch
+Plug tpope/vim-projectionist
+Plug tpope/vim-fugitive
+
+Plug junegunn/vim-easy-align
+Plug junegunn/gv.vim
+Plug junegunn/vim-slash
+Plug junegunn/vim-peekaboo
+Plug junegunn/goyo.vim
+Plug junegunn/limelight.vim
+
+Plug justinmk/vim-dirvish
+Plug justinmk/vim-sneak
 
 echo "DONE"
